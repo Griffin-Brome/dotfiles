690abdeea09d0c216ec5ec93ba48e56e7aa8156c
Griffin Brome <griffinbrome@gmail.com>
2020-09-09 16:22:00 -0700

    Write New init.vim From Scratch
    Start an init.vim configuration from a blank file. Right now only lines
    I understand have been added (I intend to keep it this way). The only
    plugin used right now is the gruvbox theme. Other changes: set $EDITOR
    variable to nvim in fish.

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,12 @@
 # Description: My fish configuration
 
 
-alias vim="nvim"
+alias vi="nvim"
 alias reload="source $HOME/.config/fish/config.fish"
 set -x DOTFILES "$HOME/.dotfiles"
+set -x EDITOR "nvim"
+
+
 function mcd -d "Create a directory and set CWD"
     command mkdir $argv
     if test $status = 0
@@ -23,11 +26,4 @@ end
 function config
     nvim "$DOTFILES/fish/config.fish"
     source "$DOTFILES/fish/config.fish"
-end
-
-
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
 end
