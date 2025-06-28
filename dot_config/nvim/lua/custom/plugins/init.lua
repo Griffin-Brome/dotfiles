b30d20f4169428f9642f7b320c824611c8802e60
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2025-06-28 09:27:29 -0700

    Init

---
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,20 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'tpope/vim-surround',
+  'tpope/vim-repeat',
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  {
+    'Exafunction/windsurf.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      require('codeium').setup {}
+    end,
+  },
+}
