1d69fb1a198595f5b4caaef723fd2db104427e8a
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2025-08-07 15:49:25 -0700

    zellij & nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = True
 
 -- Use OSC52 sequence for clipboard integration. This is nice when
 -- paired with mosh & wezterm
