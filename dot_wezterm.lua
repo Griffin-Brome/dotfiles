78bf59c22d7100c8f7ceb8d94da82ef7dacc2384
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2026-02-10 19:47:48 -0800

    some shit

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -17,6 +17,7 @@ end
 
 config.audible_bell = "Disabled"
 config.enable_kitty_keyboard = true
+config.enable_wayland = false
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 
@@ -30,6 +31,67 @@ config.keys = {
   { key = ' ', mods = 'LEADER', action = wezterm.action.QuickSelect },
   
 }
+
+function distrobox_list()
+  local distrobox_list = {}
+  local success, stdout, stderr = wezterm.run_child_process {
+    'distrobox',
+    'ls',
+    '--no-color',
+  }
+  for i, line in ipairs(wezterm.split_by_newlines(stdout)) do
+    if i > 1 then
+      local id, name, status, image = line:match '(.-)%s+|%s+(.-)%s+|%s+(.-)%s+|%s+(.-)'
+      if id and name then
+        distrobox_list[id] = {
+          id = id,
+          name = name,
+          status = status,
+          image = image
+        }
+      end
+    end
+  end
+  return distrobox_list
+end
+
+function make_distrobox_fixup_func(name)
+  return function(cmd)
+    local wrapped = {
+      'distrobox',
+      'enter',
+      '--name',
+      name,
+    }
+    if cmd.args then
+      table.insert(wrapped, '--')
+      for _, arg in ipairs(cmd.args) do
+          table.insert(wrapped, arg)
+      end
+    end
+
+    cmd.args = wrapped
+    return cmd
+  end
+end
+
+function compute_exec_domains()
+  local exec_domains = {}
+  for _, distrobox in pairs(distrobox_list()) do
+    table.insert(
+      exec_domains,
+      wezterm.exec_domain(
+        'distrobox:' .. distrobox.name,
+        make_distrobox_fixup_func(distrobox.name),
+        distrobox.image
+      )
+    )
+  end
+  return exec_domains
+end
+
+config.exec_domains = compute_exec_domains()
+
 
 -- and finally, return the configuration to wezterm
 return config
