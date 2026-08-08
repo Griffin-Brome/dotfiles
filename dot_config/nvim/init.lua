17052fac295dbc900533f3644ee156395bbde76e
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2026-08-07 17:22:40 -0700

    maintain relative buffer layout when the inhabited window is resized

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -171,6 +171,10 @@ do
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function() vim.hl.on_yank() end,
   })
+
+  vim.api.nvim_create_autocmd('VimResized', {
+    command = "wincmd ="
+  })
 end
 
 -- ============================================================
